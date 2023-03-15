da54ac0016718caf796a6bda45c2bc50117706c4
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-15 18:28:55 +0200

    Create Errors Pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import Meta from '@/utils/meta/Meta'
+
+import Heading from '@/ui/heading/Heading'
+
+const Error404 = () => {
+	return (
+		<Meta title='Page Not Found'>
+			<Heading title='404 - Page Not Found' />
+		</Meta>
+	)
+}
+
+export default Error404
---
+++ src/pages/500.tsx
@@ -0,0 +1,13 @@
+import Meta from '@/utils/meta/Meta'
+
+import Heading from '@/ui/heading/Heading'
+
+const Error500 = () => {
+	return (
+		<Meta title='Page Not Found'>
+			<Heading title='500 - Server-side error occurred' />
+		</Meta>
+	)
+}
+
+export default Error500
