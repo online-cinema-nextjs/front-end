5c0c535d3170d8cebf8d827366babe4d5429e151
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-15 12:37:17 +0200

    Create Skeleton Loader Component

--- app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
+++ app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
@@ -1,11 +1,14 @@
 import Menu from '@/components/layout/Navigation/MenuContainer/Menu'
 
 import { usePopularGenres } from './usePopularGenres'
+import SkeletonLoader from '@/ui/SkeletonLoader'
 
 const GenreMenu = () => {
 	const { isLoading, data } = usePopularGenres()
 	return isLoading ? (
-		<div className='mx-11 mb-6'>Loading..</div>
+		<div className='mx-11 mb-6'>
+			<SkeletonLoader className='h-7 mt-6' count={5} />
+		</div>
 	) : (
 		<Menu menu={{ title: 'Popular Genres', items: data || [] }} />
 	)
