0bf7c7147c45a3a3861b3c94602f96331868c7d8
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-15 11:51:53 +0200

    Create Menu Item Component

---
+++ app/components/layout/Navigation/MenuContainer/MenuItem.tsx
@@ -0,0 +1,26 @@
+import cn from 'classnames'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { FC } from 'react'
+
+import styles from './Menu.module.scss'
+import { IMenuItem } from './menu.interface'
+import MaterialIcon from '@/ui/MaterialIcon'
+
+const MenuItem: FC<{ item: IMenuItem }> = ({ item }) => {
+	const { asPath } = useRouter()
+	return (
+		<li
+			className={cn({
+				[styles.active]: asPath === item.link,
+			})}
+		>
+			<Link href={item.link}>
+				<MaterialIcon name={item.icon} />
+				<span>{item.title}</span>
+			</Link>
+		</li>
+	)
+}
+
+export default MenuItem
