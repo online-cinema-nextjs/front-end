5158838e12e47d629379f5f09c43230ac1bf39f6
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-16 23:26:53 +0200

    Create user slice and create allActions Object

--- app/store/rootReducer.ts
+++ app/store/rootReducer.ts
@@ -1,5 +1,8 @@
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
+import { reducer as userReducer } from '@/store/user/user.slice'
+
 export const reducers = {
+	user: userReducer,
 	toastr: toastrReducer,
 }
