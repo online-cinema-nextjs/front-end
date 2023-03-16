16367c573505f5ca5c2afa866f5b3436486cfa05
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-16 16:56:32 +0200

    Create Field Component

---
+++ app/components/ui/form-elements/Field.tsx
@@ -0,0 +1,21 @@
+import cn from 'classnames'
+import { forwardRef } from 'react'
+
+import styles from './Form.module.scss'
+import { IField } from './form.interface'
+
+const Field = forwardRef<HTMLInputElement, IField>(
+	({ placeholder, error, type = 'text', style, ...rest }, ref) => {
+		return (
+			<div className={cn(styles.common, styles.field)} style={style}>
+				<label>
+					<span>{placeholder}</span>
+					<input ref={ref} type={type} {...rest} />
+				</label>
+				{error && <div className={styles.error}>{error.message}</div>}
+			</div>
+		)
+	}
+)
+
+export default Field
